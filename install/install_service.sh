d89d92d7b15cc90ff3255888d635c6105b7d00b7
Andy Robb <andy@andyrobb.com>
2016-10-02 16:43:08 -0700

    Iterating on service install

--- install/install_service.sh
+++ install/install_service.sh
@@ -17,28 +17,33 @@ fi
 REALUSER=$(who am i | awk '{print $1}')
 SERVICE="systemd"
 FILENAME="charlotte.service"
-FILEPATH="../etc/$SERVICE"
+FILEPATH="$DIR/../etc/$SERVICE"
 
 
 # Configure file path if it doesn't already exist
+echo -n "Creating and setting permission on directory '$FILEPATH'  "
 mkdir -p "$FILEPATH"
 chown -R $REALUSER:$REALUSER "$FILEPATH"
 if [ "$?" -ne "0" ];
 then
-  echo 1>&2 "Unable to set permission on service file path: '$FILEPATH'"
+  echo ""
+  echo 1>&2 "Unable to set permission on service file path: '$FILEPATH'... "
   exit 1
+else
+  echo "Done"
 fi
 
 
 # Generate the service file
+echo -n "Creating service file '$FILEPATH/$FILENAME'... "
 cat << EOF > "$FILEPATH/$FILENAME"
 [Unit]
 Description=Manage Charlotte reporting daemon
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/env bash -c 'cd $PWD/..; \$PWD/bin/service-all.sh start'
-ExecStop=/usr/bin/env bash -c 'cd $PWD/..; \$PWD/bin/service-all.sh stop'
+ExecStart=/usr/bin/env bash -c 'cd $DIR/..; \$PWD/bin/service-all.sh start'
+ExecStop=/usr/bin/env bash -c 'cd $DIR/..; \$PWD/bin/service-all.sh stop'
 RemainAfterExit=yes
 
 [Install]
@@ -47,42 +52,68 @@ EOF
 
 if [ "$?" -ne "0" ];
 then
+  echo ""
   echo 1>&2 "Failed to create service script: '$FILEPATH/$FILENAME'"
   exit 1
+else
+  echo "Done"
 fi
 
 
 # Set ownership
+echo -n "Change ownership of service script: '$FILEPATH/$FILENAME'... "
 chown $REALUSER:$REALUSER "$FILEPATH/$FILENAME"
 if [ "$?" -ne "0" ];
 then
+  echo ""
   echo 1>&2 "Unable to set permission on service file: '$FILEPATH/$FILENAME'"
   exit 1
+else
+  echo "Done"
 fi
 
 
 # Link
-ln -s "$FILEPATH/$FILENAME" "/etc/systemd/system/$FILENAME"
-if [ "$?" -ne "0" ];
+echo -n "Symlink service script... "
+if [ ! -f "/etc/systemd/system/$FILENAME" ];
 then
-  echo 1>&2 "Failed to create symlink for service file in: '/etc/systemd/system/$FILENAME'"
-  exit 1
+
+  ln -s "$FILEPATH/$FILENAME" "/etc/systemd/system/$FILENAME"
+  if [ "$?" -ne "0" ];
+  then
+    echo ""
+    echo 1>&2 "Failed to create symlink for service file in: '/etc/systemd/system/$FILENAME'"
+    exit 1
+  else
+    echo "Done"
+  fi
+
+else
+  echo "Exists"
 fi
 
 
 # Enable the service
+echo -n "Enable the new service... "
 systemctl enable charlotte.service
 if [ "$?" -ne "0" ];
 then
+  echo ""
   echo 1>&2 "Failed to enable Charlotte service in Systemd"
   exit 1
+else
+  echo "Done"
 fi
 
 
 # Start the service
+echo -n "Start the service... "
 service charlotte start
 if [ "$?" -ne "0" ];
 then
+  echo ""
   echo 1>&2 "Failed to start Charlotte service via Systemd"
   exit 1
+else
+  echo "Started"
 fi
